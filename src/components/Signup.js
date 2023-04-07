faf4ddf2084d48a369e2c26e16bc0b4a310e3dfb
Prasham1710 <jainprasham17@gmail.com>
2023-04-07 14:32:56 +0530

    stying

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -22,17 +22,10 @@ const Signup = () => {
   };
 
   return (
-    <div className='max-w-[700px] mx-auto my-16 p-4'>
-      <div>
+   <div className='flex justify-center items-center h-full'>
+      <div className='mt-32 max-w-[700px] w-full mx-auto border rounded-xl bg-white p-4'>
         <h1 className='text-2xl font-bold py-2'>Sign up for a free account</h1>
-        <p className='py-2'>
-          Already have an account yet?{' '}
-          <Link to='/' className='underline'>
-            Sign in.
-          </Link>
-        </p>
-      </div>
-      <form onSubmit={handleSubmit}>
+         <form onSubmit={handleSubmit}>
         <div className='flex flex-col py-2'>
           <label className='py-2 font-medium'>Email Address</label>
           <input
@@ -49,10 +42,17 @@ const Signup = () => {
             type='password'
           />
         </div>
-        <button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>
+        <button className='border rounded-xl border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>
           Sign Up
         </button>
       </form>
+      <p className='py-2'>
+          Already have an account yet?{' '}
+          <Link to='/' className='underline'>
+            Sign in.
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
